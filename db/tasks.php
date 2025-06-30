df814210f1e0394997e5499f522adc08e12ed47b
Job Céspedes Ortiz <jobcespedes@gmail.com>
2025-06-29 20:23:47 -0600

    fix: set const name

--- db/tasks.php
+++ db/tasks.php
@@ -23,7 +23,7 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined(constant_name: 'MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die();
 
 $tasks = [
     [
